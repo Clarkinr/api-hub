59db13a5abb93a97593cc01eb608cae90f3d151a
Clarkinr <robclarkin1@gmail.com>
2022-10-27 17:09:44 +0000

    create Likes app models serailzers and views

---
+++ likes/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, permissions
+from polo_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikesSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Like.objects.all()
