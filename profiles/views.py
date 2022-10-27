52dc44486ae6c480035c4f98d052773db54c7a11
Clarkinr <robclarkin1@gmail.com>
2022-10-27 23:51:18 +0000

    add post, follower and following counts to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from polo_api.permissions import IsOwnerOrReadOnly
@@ -10,8 +11,22 @@ Shows List of all previously created profiles
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_time')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_time',
+        'owner__followed__created_time',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
