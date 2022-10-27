73187b9d643bd24d42e38483a785271ed3e5dc87
Clarkinr <robclarkin1@gmail.com>
2022-10-27 17:34:43 +0000

    create followers app, models, views and serializers

---
+++ followers/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics, permissions
+from polo_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
