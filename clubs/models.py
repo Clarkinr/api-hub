5c393d9f7282eec6433e9f08c90343f36be711d3
Clarkinr <robclarkin1@gmail.com>
2022-10-28 17:34:03 +0000

    add blank=True to fields in clubs models.

--- clubs/models.py
+++ clubs/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth.models import User
 class Club(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=90)
-    play_times = models.CharField(max_length=600)
-    location = models.URLField(max_length=200)
-    facebook = models.URLField(max_length=200)
-    instagram = models.URLField(max_length=200)
+    play_times = models.CharField(max_length=600, blank=True)
+    location = models.URLField(max_length=200, blank=True)
+    facebook = models.URLField(max_length=200, blank=True)
+    instagram = models.URLField(max_length=200, blank=True)
     image = models.ImageField(
         upload_to='images/', default='../polo-default_wsqwtv', blank=True
     )
