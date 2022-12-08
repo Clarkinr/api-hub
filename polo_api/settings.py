0ce1a4c2a3f85dad7375807674a2a297c75576f2
Clarkinr <robclarkin1@gmail.com>
2022-12-08 20:38:01 +0000

    add staticfiles storage to settings.py

--- polo_api/settings.py
+++ polo_api/settings.py
@@ -184,7 +184,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = ("cloudinary_storage.storage.StaticHashedCloudinaryStorage")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
