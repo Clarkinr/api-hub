189c2d4f2348ca2026c1775d071dfcd87dd2571e
Clarkinr <robclarkin1@gmail.com>
2023-03-07 14:57:38 +0000

    remove static files storage, static files dirs static root and media_root

--- polo_api/settings.py
+++ polo_api/settings.py
@@ -191,11 +191,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = ("cloudinary_storage.storage.StaticHashedCloudinaryStorage")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
